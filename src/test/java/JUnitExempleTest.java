85035ff2bace3716cf44845463874454d938902f
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-04 21:23:32 +0100

    add Junit Exemple Test for test

---
+++ src/test/java/JUnitExempleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class JUnitExempleTest {
+    @Test
+    public void demoTestMethod(){
+        Assertions.assertTrue(true);
+    }
+}
