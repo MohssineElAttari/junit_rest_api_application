05d0d76a2f87d16b5d4ad5d7b5c1bc3e78851030
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-13 23:01:06 +0100

    new update in book controller

--- src/main/java/com/rest/app/BookController.java
+++ src/main/java/com/rest/app/BookController.java
@@ -2,12 +2,26 @@ package com.rest.app;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
      @Autowired
     private BookRepository bookRepository;
+
+     @GetMapping("/books")
+    public List<Book> getAllBookRecord(){
+         return bookRepository.findAll();
+     }
+
+     @GetMapping(value = "{bookId}")
+    public Book getBook(@PathVariable(value = "bookId")Long bookId){
+         return bookRepository.findById(bookId).get();
+     }
 }
